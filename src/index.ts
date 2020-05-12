6d952dd4e65e958e38901448eedf17710d757428
Nick Wing <nwing@txstate.edu>
2020-05-12 16:42:29 -0500

    added a health check endpoint and support for graceful restarts in load-balanced environments

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,12 @@ type ErrorHandler = (error: Error, req: fastify.FastifyRequest, res: fastify.Fas
 export default class Server {
   protected https = false
   protected errorHandlers: ErrorHandler[] = []
+  protected healthMessage?: string
+  protected shuttingDown = false
+  protected sigHandler: (signal: any) => void
   public app: fastify.FastifyInstance
-  constructor(config: Partial<fastify.ServerOptionsAsSecureHttp2> = {}) {
+
+  constructor (config: Partial<fastify.ServerOptionsAsSecureHttp2> = {}) {
     try {
       const key = fs.readFileSync('/securekeys/private.key')
       const cert = fs.readFileSync('/securekeys/cert.pem')
@@ -24,11 +28,12 @@ export default class Server {
     } catch (e) {
       this.https = false
     }
-    if (!config.logger) config.logger = true
+    if (typeof config.logger === 'undefined') config.logger = true
     this.app = fastify(config)
     this.app.addHook('onSend', async (req, resp, payload) => {
       resp.removeHeader('X-Powered-By')
     })
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
     this.app.setErrorHandler(async (err, req, res) => {
       for (const errorHandler of this.errorHandlers) {
         if (!res.sent) await errorHandler(err, req, res)
@@ -41,6 +46,16 @@ export default class Server {
         }
       }
     })
+    this.app.get('/health', async (req, res) => {
+      if (this.shuttingDown) res.status(503).send('Service is shutting down/restarting.')
+      else if (this.healthMessage) res.status(500).send(this.healthMessage)
+      else res.status(200).send('OK')
+    })
+    this.sigHandler = () => {
+      this.close().then(() => {
+        process.exit()
+      }).catch(e => console.error(e))
+    }
   }
 
   public async start (port?: number) {
@@ -50,21 +65,38 @@ export default class Server {
     } else if (this.https) {
       // redirect 80 to 443
       http.createServer((req, res) => {
-        res.writeHead(301, { Location: 'https://' + req?.headers?.host?.replace(/:\d+$/, '') + req.url })
+        res.writeHead(301, { Location: 'https://' + (req?.headers?.host?.replace(/:\d+$/, '') ?? '') + (req.url ?? '') })
         res.end()
       }).listen(80)
       await this.app.listen(443, '::')
     } else {
       await this.app.listen(80, '::')
     }
+    process.on('SIGTERM', this.sigHandler)
+    process.on('SIGINT', this.sigHandler)
   }
 
   public async addErrorHandler (handler: ErrorHandler) {
     this.errorHandlers.push(handler)
   }
 
-  public async close () {
-    return this.app.server.close()
+  public setUnhealthy (message: string) {
+    this.healthMessage = message
+  }
+
+  public setHealthy () {
+    this.healthMessage = undefined
+  }
+
+  public async close (softSeconds?: number) {
+    if (typeof softSeconds === 'undefined') softSeconds = parseInt(process.env.LOAD_BALANCE_TIMEOUT ?? '0')
+    process.removeListener('SIGTERM', this.sigHandler)
+    process.removeListener('SIGINT', this.sigHandler)
+    if (softSeconds) {
+      this.shuttingDown = true
+      await new Promise(resolve => setTimeout(resolve, softSeconds! * 1000))
+    }
+    await this.app.close()
   }
 }
 
