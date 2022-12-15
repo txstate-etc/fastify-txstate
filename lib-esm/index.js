0c86b9c83f92c16fd4bcb8d707ae355a3868333f
Nick Wing <nwing@txstate.edu>
2022-12-15 13:49:39 -0600

    export devLogger and prodLogger in ESM, release 3.1.3

--- lib-esm/index.js
+++ lib-esm/index.js
@@ -1,5 +1,7 @@
 import ftxst from '../lib/index.js'
 
+export const devLogger = ftxst.devLogger
+export const prodLogger = ftsxt.prodLogger
 export const HttpError = ftxst.HttpError
 export const FailedValidationError = ftxst.FailedValidationError
 export default ftxst.default
